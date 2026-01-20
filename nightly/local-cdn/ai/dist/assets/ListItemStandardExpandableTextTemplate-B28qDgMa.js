fd3158d8ae349696d52eded6f65adc17fb5b45cb
kchobantonov <kchobantonov@users.noreply.github.com>
2026-01-20 02:24:20 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@b62c313d4ba1f2baa1feb048066eb90c5c4f7571 🚀

---
+++ nightly/local-cdn/ai/dist/assets/ListItemStandardExpandableTextTemplate-B28qDgMa.js
@@ -0,0 +1,1 @@
+import{a as x,E as m}from"./bundle.esm-9x9-Q6yh.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
