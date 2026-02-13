38cd26eb035295e9428eee99d6d98b2c777e55fd
kchobantonov <kchobantonov@users.noreply.github.com>
2026-02-13 02:56:49 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@b62c313d4ba1f2baa1feb048066eb90c5c4f7571 🚀

---
+++ nightly/assets/ListItemStandardExpandableTextTemplate-Dr8GWerp.js
@@ -0,0 +1,1 @@
+import{j as x,aw as m}from"./bundle.esm-sz04F6B4.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1770864728,
+    buildTime: 1770951144,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
