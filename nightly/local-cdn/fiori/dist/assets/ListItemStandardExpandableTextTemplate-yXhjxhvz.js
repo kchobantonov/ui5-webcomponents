9a2874ef60904f6c54257254ccd57bf22bb0f602
kchobantonov <kchobantonov@users.noreply.github.com>
2026-02-21 02:40:12 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@b62c313d4ba1f2baa1feb048066eb90c5c4f7571 🚀

--- nightly/local-cdn/fiori/dist/assets/ListItemStandardExpandableTextTemplate-yXhjxhvz.js
+++
@@ -1,1 +0,0 @@
-import{j as x,aw as m}from"./bundle.esm-a1Fki7zp.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
