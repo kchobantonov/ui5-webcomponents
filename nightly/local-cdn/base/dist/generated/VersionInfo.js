0c1ecee98260a26f7c1952386ea5ed3f512afec4
kchobantonov <kchobantonov@users.noreply.github.com>
2026-04-16 03:38:20 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@1500ada0086b5e049f3e719245e05fffbf3704ba 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "",
     isNext: false,
-    buildTime: 1776223519,
+    buildTime: 1776310436,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
