a79af7910b50df6cded060d5b45b73401f3566f1
kchobantonov <kchobantonov@users.noreply.github.com>
2026-08-04 03:25:06 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@1500ada0086b5e049f3e719245e05fffbf3704ba 🚀

---
+++ nightly/local-cdn/main/dist/assets/test/pages/Icon.html-BpHVG6k1.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-J4PhwAFA.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
