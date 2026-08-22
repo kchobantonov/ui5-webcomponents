8ea44f9229a358ddf95aa6d2bb0459a54a388edd
kchobantonov <kchobantonov@users.noreply.github.com>
2026-08-22 01:43:15 +0000

    Deploying to gh-pages from @ kchobantonov/ui5-webcomponents@1500ada0086b5e049f3e719245e05fffbf3704ba 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-BuSB_PEQ.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-12ynPOLg.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "",
     isNext: false,
-    buildTime: 1787276649,
+    buildTime: 1787362673,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
